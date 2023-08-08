b55fc0d26dac008a5c649d39019e4e68ad13d257
boban.alempijevic <boban.alempijevic@gmail.com>
2023-08-08 22:20:04 +0300

    started building the Onboarding page

---
+++ components/forms/AccountProfile.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+interface Props {
+    user: {
+        id: string;
+        objectId: string;
+        username: string;
+        name: string;
+        bio: string;
+        image: string;
+    };
+    btnTitle: string;
+}
+
+const AccountProfile = ({ user, btnTitle }:
+    Props) => {
+    return (
+        <div className="text-light-1">
+            Account Profile
+        </div>
+    )
+}
+
+export default AccountProfile;
--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { OrganizationSwitcher, SignedIn, SignOutButton } from "@clerk/nextjs";
+import { dark } from '@clerk/themes';
 
 function TopBar() {
     return (
@@ -29,6 +30,7 @@ function TopBar() {
                 </div>
                 <OrganizationSwitcher 
                     appearance={{
+                        baseTheme: dark,
                         elements: {
                             organizationSwitcherTrigger:
                             "py-2 px-4"
